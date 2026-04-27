e79cc618a6915433711b4e891d3c40f2be46de70
Will Droste <will.droste@brinqa.com>
2026-04-26 22:55:54 -0500

    Fix format string UB and wrong error variable in extension code
    Bug 1: extension_load.cpp line 697 used %s for a uint8_t argument,
    which is undefined behavior. Changed to %d to match the identical
    pattern at line 129.
    
    Bug 2: extension_install.cpp line 312 reported temp_path in an error
    message about local_extension_path. Copy-paste error from the block
    above.
    
    Ref: https://github.com/duckdb/duckdb/pull/21861

--- src/main/extension/extension_install.cpp
+++ src/main/extension/extension_install.cpp
@@ -309,7 +309,7 @@ static void WriteExtensionFiles(QueryContext &query_context, FileSystem &fs, con
 	if (!StringUtil::EndsWith(local_extension_path, ".duckdb_extension")) {
 		throw InternalException("Extension install local_extension_path of '%s' is not valid, should end in "
 		                        "'.duckdb_extension'",
-		                        temp_path);
+		                        local_extension_path);
 	}
 
 	// Write extension to tmp file
